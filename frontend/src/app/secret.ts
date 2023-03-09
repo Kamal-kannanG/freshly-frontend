3e7ad671c495b598fd7edd5024639ee4fdbaece2
Kamal-kannanG <111959395+Kamal-kannanG@users.noreply.github.com>
2023-03-09 21:45:37 +0530

    deployyy

---
+++ frontend/src/app/secret.ts
@@ -0,0 +1,10 @@
+
+//     export const data:{
+//     MID = "v";
+//     WEBSITE: WEBSITE,
+//     INDUSTRY_TYPE_ID: INDUSTRY_TYPE_ID,
+//     CHANNEL_ID: CHANNEL_ID,
+//     CALLBACK_URL: CALLBACK_URL
+// }
+
+export const data: any = "hello";
